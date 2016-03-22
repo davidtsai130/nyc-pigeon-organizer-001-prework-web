66745ab1f241db32009ab8c15104b693c0f5f1dd
davidtsai130 <davidtsai130@gmail.com>
2016-03-22 01:25:52 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,23 +1,23 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
   hash = {}
-  
+
   data.each do |trait, trait_info|
     trait_info.each do |info, names|
       names.each do |name|
         if hash.keys.include?(name)
           if hash[name].keys.include?(trait)
-            hash[name][trait] << trait_info.to_s
+            hash[name][trait] << info.to_s
           else
-            hash[name][trait] = [trait_info.to_s]
+            hash[name][trait] = [info.to_s]
           end
         else
-        hash[name] = {trait => [trait_info.to_s]}
+        hash[name] = {trait => [info.to_s]}
         end
       end
     end
   end
-  
+
   hash
 end
 
